e9b1b34bde4ce7ef86a82b3b4daf8bf8cab8e09c
Strabox <pardal.pires@gmail.com>
2017-06-20 14:26:54 +0100

    Add project files.

---
+++ ArduinoControllerSerra/ArduinoControllerSerra.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		ArduinoControllerSerra.ino
+ Created:	6/20/2017 2:26:40 PM
+ Author:	André
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
